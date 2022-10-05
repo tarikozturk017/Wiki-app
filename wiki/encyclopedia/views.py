71592f3914dddaa9dfc4349e2de16300c1f2a4f6
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-10-05 10:22:52 -0400

    Search functionality added

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from markdown2 import Markdown
 from django.http import Http404
+from django import forms
+from django.urls import reverse
+
 
 from . import util
+#from wiki import encyclopedia
 
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    if content == None:
+        return None
+    else:
+        return Markdown().convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -21,5 +31,21 @@ def title(request, title):
         "content": Markdown().convert(data)
     } )
 
-
-    
+def search(request):
+    if request.method == "POST":
+        search_input = request.POST['q'] # looks for name="q" in layout  
+        html_content = convert_md_to_html(search_input) #md -> html 
+        if html_content is not None:
+            return render(request, "encyclopedia/title.html", {
+                "title": search_input,
+                "content": html_content
+            })
+        else:
+            listEntries = util.list_entries()
+            foundTitles = []
+            for title in listEntries:
+                if title.find(search_input) != -1:
+                    foundTitles.append(title)
+            return render(request, "encyclopedia/search.html", {
+                "searches": foundTitles
+            })
